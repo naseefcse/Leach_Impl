bfb50c967bf65beee56672d9205f70b39a1b8b5b
Naseef Ur Rahman <naseefcse@gmail.com>
2019-05-07 02:04:55 -0600

    Initial Commit

---
+++ leach.h
@@ -0,0 +1,20 @@
+/*
+ *  Implementation of Mohammad Hossein Homaei's LEACH simulator, in C.
+ *  Joseph Finnegan
+ *  joseph.finnegan@cs.nuim.ie
+ *  2017
+ */
+
+#include "../network/clusterModel.h"
+#include <math.h>
+#include <stdio.h>
+
+#ifndef LEACH_H
+#define LEACH_H
+
+struct ClusterModel* leach(ClusterModel* clusterM, int nodeId);
+
+#endif /* LEACH_H */
+
+
+
